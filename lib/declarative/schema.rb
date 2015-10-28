ba7cbee10d86850adfcd709b9f1349ad5e40587d
Nick Sutterer <apotonick@gmail.com>
2015-10-28 21:53:40 +1100

    add Schema::feature.

--- lib/declarative/schema.rb
+++ lib/declarative/schema.rb
@@ -1,4 +1,8 @@
 module Declarative
+  # Include this to maintain inheritable, nested schemas with ::defaults and
+  # ::feature the way we have it in Representable, Reform, and Disposable.
+  #
+  # The schema with its defnitions will be kept in ::definitions.
   module Schema
     module DSL
       def property(name, options={}, &block)
@@ -21,6 +25,26 @@ module Declarative
         @definitions ||= Definitions.new(Definitions::Definition)
       end
     end
+
+    module Feature
+      # features are registered as defaults using :include_modules, which in turn get translated to
+      # Class.new... { feature mod } which makes it recursive in nested schemas.
+      def feature(*mods)
+        mods.each do |mod|
+          include mod
+          register_feature(mod)
+        end
+      end
+
+    private
+      def register_feature(mod)
+        # heritage.record(:register_feature, mod) # this is only for inheritance between decorators and modules!!! ("horizontal and vertical")
+
+        defaults[:include_modules] ||= []
+        defaults[:include_modules] << mod
+      end
+    end
+
 
     NestedBuilder = ->(options) {
       base = Class.new(options[:base])
--- test/schema_test.rb
+++ test/schema_test.rb
@@ -4,13 +4,21 @@ require "declarative/schema"
 class SchemaTest < Minitest::Spec
   class Decorator
     extend Declarative::Schema::DSL
+    extend Declarative::Schema::Feature # TODO: make automatic
 
     def self.default_nested_class
       Decorator
     end
   end
 
+  module AddToOptions
+    def self.included(includer)
+      includer.definitions.each { |dfn| dfn.options[:added] = true }
+    end
+  end
+
   class Concrete < Decorator
+    feature AddToOptions
     defaults render_nil: true do |name|
       { as: name.to_s.upcase }
     end
@@ -29,7 +37,6 @@ class SchemaTest < Minitest::Spec
   it do
     Concrete.extend(Inspect::Schema)
     Concrete.inspect
-    # pp Concrete.definitions.extend(Definitions::Inspect)
     pp Concrete.inspect
     Concrete.inspect.must_equal 'Schema: {"artist"=>#<Declarative::Definitions::Definition: @options={:render_nil=>true, :as=>"ARTIST", :cool=>true, :nested=>Schema: {"name"=>#<Declarative::Definitions::Definition: @options={}, @name="name">, "band"=>#<Declarative::Definitions::Definition: @options={:crazy=>nil, :nested=>Schema: {"location"=>#<Declarative::Definitions::Definition: @options={}, @name="location">}}, @name="band">}}, @name="artist">, "id"=>#<Declarative::Definitions::Definition: @options={:render_nil=>true, :as=>"ID", :unique=>true, :value=>1}, @name="id">}'
   end
