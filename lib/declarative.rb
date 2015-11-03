067a148331e6f40141399c5691808a0cb248972e
Nick Sutterer <apotonick@gmail.com>
2015-11-03 13:58:23 +1100

    restructure files.

--- lib/declarative.rb
+++ lib/declarative.rb
@@ -2,9 +2,21 @@ require "declarative/version"
 require "declarative/definitions"
 require "declarative/defaults"
 require "declarative/schema"
+require "declarative/deep_dup"
 
 module Declarative
   class Heritage < Array
+    def record(method, *args, &block)
+      self << {method: method, args: DeepDup.(args), block: block} # DISCUSS: options.dup.
+    end
+
+    def call(inheritor)
+      each do |cfg|
+        inheritor.send(cfg[:method], *cfg[:args], &cfg[:block])
+      end
+    end
+
+
     module DSL
       def heritage
         @heritage ||= Heritage.new
@@ -23,30 +35,6 @@ module Declarative
         super
         heritage.(mod)
       end
-    end
-
-
-    def record(method, *args, &block)
-      self << {method: method, args: DeepDup.(args), block: block} # DISCUSS: options.dup.
-    end
-
-    def call(inheritor)
-      each do |cfg|
-        inheritor.send(cfg[:method], *cfg[:args], &cfg[:block])
-      end
-    end
-  end
-
-  class DeepDup
-    def self.call(args)
-      return Array[*dup_items(args)] if args.is_a?(Array)
-      return Hash[dup_items(args)] if args.is_a?(Hash)
-      args
-    end
-
-  private
-    def self.dup_items(arr)
-      arr.to_a.collect { |v| call(v) }
     end
   end
 end
